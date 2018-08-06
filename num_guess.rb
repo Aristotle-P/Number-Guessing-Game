293aca6ebe91836efbdd24f2eec4535f4d199049
Aristotle Poultney <aristotlepoultney@gmail.com>
2018-08-06 17:08:23 -0400

    completed guessing game

--- num_guess.rb
+++ num_guess.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-num = rand(101)
+NUM = rand(10)
+
+def guess_check(guess)
+    guess = guess.to_i
+    if guess >= NUM + 5
+        message = 'Way too high!'
+    elsif guess <= NUM - 5
+        message = 'Way too low!'
+    elsif guess == NUM
+        message = 'You got it!'
+    elsif guess > NUM
+        message = 'Too high!'
+    elsif guess < NUM
+        message = 'Too low!'
+    end
+end
 
 get '/' do
-    erb :index, :locals => {:num => num}
+    guess = params['guess']
+    message = guess_check(guess)
+    erb :index, :locals => {:num => NUM, :message => message}
 end
